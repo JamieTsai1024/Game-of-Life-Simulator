ddc9cb2836b2a88dfbcc3de8e0661d7059411509
Jamie1024 <jamie.tsai88@gmail.com>
2022-06-18 19:57:00 -0400

    Created navbar

--- game-of-life/src/App.js
+++ game-of-life/src/App.js
@@ -1,26 +1,38 @@
-import React, { useState, useEffect } from "react";
+import React, { Component } from "react";
+import NavigationBar from "./components/NavigationBar";
+import {
+  solid,
+  // regular,
+  brands,
+} from "@fortawesome/fontawesome-svg-core/import.macro"; // <-- import styles to be used
 
-function App() {
-  const [data, setData] = useState([{}]);
-
-  useEffect(() => {
-    fetch("/members")
-      .then((res) => res.json())
-      .then((data) => {
-        setData(data);
-        console.log(data);
-      });
-  }, []);
-  // data is the actual variable and setData is the function we can use to manipulate the state of this data
-  return (
-    <div>
-      {typeof data.members == "undefined" ? (
-        <p>Loading...</p>
-      ) : (
-        data.members.map((member, i) => <p key={i}>{member}</p>)
-      )}
-    </div>
-  );
+class App extends Component {
+  state = {
+    links: [
+      {
+        id: 0,
+        page: "Explanation",
+        openInNewTab: false,
+        url: "#explanation",
+        icon: solid("circle-info"),
+      },
+      {
+        id: 1,
+        page: "GitHub",
+        openInNewTab: true,
+        url: "https://github.com/JamieTsai1024/Game-of-Life-Simulator",
+        // icon: solid("user-secret"),
+        icon: brands("github"),
+      },
+    ],
+  };
+  render() {
+    return (
+      <React.Fragment>
+        <NavigationBar links={this.state.links} />
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
