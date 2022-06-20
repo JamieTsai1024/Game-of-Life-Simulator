7698b82b5fed4af35bbf7729143e161c828016fc
Jamie1024 <jamie.tsai88@gmail.com>
2022-06-20 18:07:11 -0400

    Styled board

--- game-of-life/src/components/Board.jsx
+++ game-of-life/src/components/Board.jsx
@@ -2,15 +2,24 @@ import React from "react";
 import Square from "./Square";
 
 function Board(props) {
+  const boardStyle = {
+    // border: "solid black 10px",
+    backgroundColor: "black",
+    height: "100%",
+  };
+
   return (
-    <div>
-      {props.board.map((row, index) => (
-        <div key={index} className="d-flex align-items-center">
-          {row.map((squareActive, ind) => (
+    <div style={boardStyle}>
+      {/* Having trouble centering the board */}
+      {/* <div className="row d-flex justify-content-center"> */}
+      {/* doesn't work for centering board: className="row d-flex justify-content-center" */}
+      {props.board.map((row, rowIndex) => (
+        <div key={rowIndex} className="d-flex align-items-center">
+          {row.map((squareActive, colIndex) => (
             <Square
-              key={index + " " + ind}
-              row={index}
-              col={ind}
+              key={rowIndex + " " + colIndex}
+              row={rowIndex}
+              col={colIndex}
               size="50px"
               onSquareClick={props.onSquareClick}
               colour={squareActive ? props.liveColour : props.deadColour}
@@ -19,6 +28,7 @@ function Board(props) {
         </div>
       ))}
     </div>
+    // </div>
   );
 }
 
