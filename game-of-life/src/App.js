ea1422e7538fb01c54ae345e375560c97d2c827e
Jamie1024 <jamie.tsai88@gmail.com>
2022-06-20 23:40:31 -0400

    Removed comments

--- game-of-life/src/App.js
+++ game-of-life/src/App.js
@@ -31,111 +31,40 @@ class App extends Component {
       ],
       start: true,
       reset: false,
-      board: [
-        [0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0],
-      ],
       // board: [
-      //   [0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0],
-      //   [0, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0],
-      //   [0, 1, 1, 1, 0, 0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0],
-      //   [0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0],
-      //   [0, 1, 1, 0, 0, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0],
-      //   [0, 1, 1, 0, 0, 1, 0, 0, 1, 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 0],
-      //   [0, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0],
-      //   [0, 1, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0],
-      //   [0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0],
-      //   [0, 1, 1, 0, 0, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0],
-      //   [0, 1, 1, 0, 0, 1, 0, 0, 1, 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 0],
-      //   [0, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0],
-      //   [0, 1, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0],
+      //   [0, 0, 0, 0, 0],
+      //   [0, 0, 0, 0, 0],
+      //   [0, 0, 0, 0, 0],
+      //   [0, 0, 0, 0, 0],
+      //   [0, 0, 0, 0, 0],
       // ],
+      board: [
+        [0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0],
+        [0, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0],
+        [0, 1, 1, 1, 0, 0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0],
+        [0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0],
+        [0, 1, 1, 0, 0, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0],
+        [0, 1, 1, 0, 0, 1, 0, 0, 1, 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 0],
+        [0, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0],
+        [0, 1, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0],
+        [0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0],
+        [0, 1, 1, 0, 0, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0],
+        [0, 1, 1, 0, 0, 1, 0, 0, 1, 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 0],
+        [0, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0],
+        [0, 1, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0],
+      ],
       originalBoard: [],
       step: 0,
     };
   }
 
-  // nextStep = (board) => {
-  //   // Clone the array with ES6
-  //   const resultBoard = [...board];
-  //   var surroundingLive = 0;
-
-  //   for (var i = 0; i < board.length; i++) {
-  //     for (var j = 0; j < board[0].length; j++) {
-  //       // Calculate how many surrounding squares are alive
-  //       surroundingLive = 0;
-  //       if (i !== 0 && board[i]) {
-  //         // Look through the top
-  //         // Top right
-  //         surroundingLive += j !== 0 ? board[i - 1][j - 1] : 0;
-  //         // Top middle
-  //         surroundingLive += board[i - 1][j];
-  //         // Top left
-  //         surroundingLive +=
-  //           j + 1 !== board[0].length - 1 ? board[i - 1][j + 1] : 0;
-  //       }
-
-  //       if (i !== board.length - 1) {
-  //         // Look through the bottom
-  //         // Bottom right
-  //         surroundingLive += j !== 0 ? board[i + 1][j - 1] : 0;
-  //         // Bottom middle
-  //         surroundingLive += board[i + 1][j];
-  //         // Bottom left
-  //         surroundingLive +=
-  //           j + 1 !== board[0].length - 1 ? board[i + 1][j + 1] : 0;
-  //       }
-
-  //       if (j !== 0) {
-  //         // Middle left
-  //         surroundingLive += board[i][j - 1];
-  //       }
-
-  //       if (j !== board[0].length - 1) {
-  //         // Middle right
-  //         surroundingLive += board[i][j + 1];
-  //       }
-
-  //       console.log("i, j", i, j, "surroundingLive", surroundingLive);
-
-  //       // Simplify these cases later
-  //       if (board[i][j]) {
-  //         // Case 1: The cell is alive
-  //         if (surroundingLive === 2 || surroundingLive === 3) {
-  //           // Should live
-  //           resultBoard[i][j] = 1;
-  //         } else {
-  //           resultBoard[i][j] = 0;
-  //         }
-  //       } else {
-  //         // Case 2: The cell is dead
-  //         if (surroundingLive === 3) {
-  //           // Should live
-  //           resultBoard[i][j] = 1;
-  //         } else {
-  //           resultBoard[i][j] = 0;
-  //         }
-  //       }
-  //     }
-  //   }
-  //   // Code this function
-  //   return resultBoard;
-  // };
-
   handleNext = () => {
-    console.log("handlenext");
-    // Code this function
-    // const board = this.state.board;
-    // Clone the array with ES6
-    // var resultBoard = [...this.state.board];
     var resultBoard = this.state.board.map(function (arr) {
       return arr.slice();
     });
+    // maybe make a variable for this.state.board
 
-    console.log("this.state.board", this.state.board);
+    // console.log("this.state.board", this.state.board);
     var surroundingLive = 0;
 
     for (var i = 0; i < this.state.board.length; i++) {
@@ -146,11 +75,10 @@ class App extends Component {
           // Look through the top
           // Top left
           surroundingLive += j !== 0 ? this.state.board[i - 1][j - 1] : 0;
-          console.log("here i, j", i, j, "Top left", surroundingLive);
-
+          // console.log("here i, j", i, j, "Top left", surroundingLive);
           // Top middle
           surroundingLive += this.state.board[i - 1][j];
-          console.log("here i, j", i, j, "Top left", surroundingLive);
+          // console.log("here i, j", i, j, "Top left", surroundingLive);
           // Top right
           surroundingLive +=
             j !== this.state.board[0].length - 1
@@ -158,7 +86,7 @@ class App extends Component {
                 ? 1
                 : 0
               : 0;
-          console.log("here i, j", i, j, "Top left", surroundingLive);
+          // console.log("here i, j", i, j, "Top left", surroundingLive);
         }
 
         if (j !== 0) {
@@ -166,30 +94,23 @@ class App extends Component {
           surroundingLive += this.state.board[i][j - 1];
         }
 
-        console.log(
-          "here i, j",
-          i,
-          j,
-          "Middle left",
-          surroundingLive,
-          this.state.board[i][j - 1]
-        );
+        // console.log("here i, j", i, j, "Middle left", surroundingLive);
 
         if (j !== this.state.board[0].length - 1) {
           // Middle right
           surroundingLive += this.state.board[i][j + 1];
         }
 
-        console.log("here i, j", i, j, "Middle right", surroundingLive);
+        // console.log("here i, j", i, j, "Middle right", surroundingLive);
 
         if (i !== this.state.board.length - 1) {
           // Look through the bottom
           // Bottom left
           surroundingLive += j !== 0 ? this.state.board[i + 1][j - 1] : 0;
-          console.log("here i, j", i, j, "Bottom left", surroundingLive);
+          // console.log("here i, j", i, j, "Bottom left", surroundingLive);
           // Bottom middle
           surroundingLive += this.state.board[i + 1][j];
-          console.log("here i, j", i, j, "Bottom middle", surroundingLive);
+          // console.log("here i, j", i, j, "Bottom middle", surroundingLive);
           // Bottom right
           surroundingLive +=
             j !== this.state.board[0].length - 1
@@ -197,7 +118,7 @@ class App extends Component {
                 ? 1
                 : 0
               : 0;
-          console.log("here i, j", i, j, "Bottom right", surroundingLive);
+          // console.log("here i, j", i, j, "Bottom right", surroundingLive);
         }
 
         console.log("i, j", i, j, "total", surroundingLive);
@@ -209,12 +130,11 @@ class App extends Component {
           if (surroundingLive !== 2 && surroundingLive !== 3) {
             // Should die
             console.log(
-              this.state.board[i][j],
+              // this.state.board[i][j],
               "i, j",
               i,
               j,
-              "turn off - surroundingLive",
-              surroundingLive
+              "turn off - surroundingLive"
             );
             resultBoard[i][j] = 0;
           }
@@ -224,44 +144,17 @@ class App extends Component {
           if (surroundingLive === 3) {
             // Should live
             resultBoard[i][j] = 1;
-            console.log(
-              "i, j",
-              i,
-              j,
-              "turn on - surroundingLive",
-              surroundingLive
-            );
+            console.log("i, j", i, j, "turn on - surroundingLive");
           }
         }
-
-        // Original
-        // if (this.state.board[i][j]) {
-        //   // Case 1: The cell is alive
-        //   if (surroundingLive === 2 || surroundingLive === 3) {
-        //     // Should live
-        //     resultBoard[i][j] = 1;
-        //     console.log("i, j", i, j, "surroundingLive", surroundingLive);
-        //   } else {
-        //     resultBoard[i][j] = 0;
-        //   }
-        // } else {
-        //   // Case 2: The cell is dead
-        //   if (surroundingLive === 3) {
-        //     // Should live
-        //     resultBoard[i][j] = 1;
-        //     console.log("i, j", i, j, "surroundingLive", surroundingLive);
-        //   } else {
-        //     resultBoard[i][j] = 0;
-        //   }
-        // }
       }
     }
     this.setState({ board: resultBoard });
   };
 
   handleStart = () => {
-    console.log("start");
     // Save current board for reset and toggle start/pause
+    console.log("start");
     this.setState({
       originalBoard: this.state.board,
       start: !this.state.start,
@@ -312,7 +205,6 @@ class App extends Component {
           deadColour="#5ffae0"
           onSquareClick={this.handleSquareClick}
         />
-        {/* <Square colour="#1B5C41" onSquareClick={this.handleSquareClick} /> */}
         <OptionsBar
           start={this.state.start}
           reset={this.state.reset}
@@ -320,18 +212,6 @@ class App extends Component {
           onReset={this.handleReset}
           onNext={this.handleNext}
         />
-        {/* <div>
-          {this.state.board.map((row, rowIndex) => (
-            <div key={rowIndex} className="d-flex align-items-center">
-              {row.map((squareActive, colIndex) => (
-                <div key={rowIndex + " " + colIndex}>
-                  h{squareActive.toString()}a{squareActive === true}m
-                  {squareActive | 0}
-                </div>
-              ))}
-            </div>
-          ))}
-        </div> */}
       </React.Fragment>
     );
   }
